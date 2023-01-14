ddc15f097ad1beecc8099387445b0a4f378b49cf
Татьяна Яблонская <111698588+YablonskaTanya@users.noreply.github.com>
2023-01-14 22:39:32 +0200

    add tack 11-20

--- js/hw_3.js
+++ js/hw_3.js
@@ -161,16 +161,159 @@
 //   values.push(apartment[key]);
 // }
 
-// 11;
-// 12;
-// 13;
-// 14;
-// 15;
-// 16;
-// 17;
-// 18;
-// 19;
-// 20;
+// // 11;
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Spacious apartment in the city center';
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+
+//   // Change code above this line
+// }
+
+// // 12;
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+//   // Change code above this line
+//   return propCount;
+// }
+
+// // 13;
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// // 14;
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+//   const keys = Object.keys(object);
+//   for (const key of keys) {
+//     propCount += 1;
+//   }
+
+//   return propCount;
+//   // Change code above this line
+// }
+
+// // 15;
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+
+// // 16;
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   for (const salary of Object.values(salaries)) {
+//     totalSalary += salary;
+//   }
+//   // Change code above this line
+//   return totalSalary;
+// }
+
+// // 17;
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (let color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+
+// // 18;
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products)
+//     if (product.name === productName) return product.price;
+//   return null;
+
+//   // Change code above this line
+// }
+
+// // 19;
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   const getAllProp = [];
+//   products.forEach(item => {
+//     if (item.hasOwnProperty(propName)) {
+//       getAllProp.push(item[propName]);
+//     }
+//   });
+//   return getAllProp;
+// }
+// // 20;
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     const totalPrice = product.price * product.quantity;
+//     if (productName === product.name) {
+//       return totalPrice;
+//     }
+//   }
+//   return 0;
+
+//   // Change code above this line
+// }
+
 // 21;
 // 22;
 // 23;
