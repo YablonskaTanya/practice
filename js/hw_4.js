de4830fe03075d0ad9412a48d66ad910c3b04c63
Татьяна Яблонская <111698588+YablonskaTanya@users.noreply.github.com>
2023-01-18 22:42:22 +0200

    add task 1

--- js/hw_4.js
+++ js/hw_4.js
@@ -1,1 +1,8 @@
 //tasks/1
+function makePizza() {
+  return 'Your pizza is being prepared, please wait.';
+}
+// Change code below this line
+
+const result = makePizza();
+const pointer = makePizza;
