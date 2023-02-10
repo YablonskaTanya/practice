28e4ef61caab3a5f01b5d25b3e759fe1435de017
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-02-10 23:36:46 +0200

    added task from practice

---
+++ js/hw/hw-1-5.js
@@ -0,0 +1,341 @@
+// const massage = prompt('#hello');
+// console.log(Number(massage));
+
+// console.log(confirm('are you shere'));
+
+// const valueA = '5';
+// console.log(Number(valueA)); // 5
+// console.log(typeof Number(valueA)); // "number"
+
+// const valueB = 'random string';
+// console.log(Number(valueB)); // NaN
+// console.log(typeof Number(valueB)); // "number"
+
+// console.log(50 + 'ewe');
+
+// for (let i = 0; i < 10; i += 1) {
+//   console.log(i);
+// }
+
+// const userInput = prompt('hello');
+
+// if (!userInput) {
+//   alert('ERROR');
+// }
+// if (userInput === '1') {
+//   alert('OK');
+// } else {
+//   alert('NO');
+// }
+
+// const a = 5 + '5';
+// console.log(typeof a);
+// console.log(Number(5 + 5 + '5'));
+// console.log(55 - '5');
+// console.log(5 + 'hello');
+// console.log(5 - 'hello');
+
+// const clients = ['Mango', 'Ajax', 'Poly'];
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
+// const clients = ['Mango', 'Ajax', 'Poly'];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+// const string = 'javascript';
+
+// for (const character of string) {
+//   console.log(character);
+// }
+
+// const clients = ['Mango', 'Poly', 'Ajax'];
+// const clientNameToFind = 'Poly';
+// let message;
+
+// for (const client of clients) {
+//   // На кожній ітерації будемо перевіряти чи збігається елемент масиву з
+//   // іменем клієнта. Якщо збігається - записуємо в message повідомлення
+//   // про успіх і робимо break, щоб далі не шукати
+//   if (client === clientNameToFind) {
+//     message = "Клієнт з таким ім'ям є в базі даних!";
+//     break;
+//   }
+
+//   // Якщо вони не збігаються - записуємо в message повідомлення про відсутність імені
+//   message = "Клієнт з таким ім'ям відсутній в базі даних!";
+// }
+
+// console.log(message); // "Клієнт з таким ім'ям є в базі даних!"
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.pop()); //  5
+// console.log(numbers); // [1, 2, 3, 4]
+
+// console.log(numbers.pop()); //  4
+// console.log(numbers); // [1, 2, 3]
+
+// console.log(numbers.pop()); //  3
+// console.log(numbers); // [1, 2]
+
+// console.log(numbers.pop()); //  2
+// console.log(numbers); // [1]
+
+// console.log(numbers.pop()); //  1
+// console.log(numbers); // []
+
+// console.log(numbers.pop()); //  undefined
+// console.log(numbers); // []
+
+// const clients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// console.log(clients.slice(1, 3)); // ["Ajax", "Poly"]
+// console.log(clients);
+// let clients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// let newArray = clients.slice();
+// let array = clients;
+
+// console.log(newArray);
+// console.log(clients);
+// console.log(newArray === clients);
+// console.log(array === clients);
+
+// const scores = [1, 2, 3, 4, 5];
+
+// // Видаляємо три елементи масиву, починаючи з першого елемента (індекс 0)
+// const deletedScores = scores.splice(0, 3);
+
+// // Тепер масив scores містить два елементи
+// console.log(scores); // [4, 5]
+
+// // А масив deletedScores містить три видалені елементи
+// console.log(deletedScores); // [1, 2, 3]
+
+// const colors = ['red', 'green', 'blue'];
+// colors.splice(2, 0, 'purple');
+// console.log(colors); // ["red", "green", "purple", "blue"]
+
+// 1. Оголошення параметрів x, y, z
+// function multiply(x, y, z) {
+//   console.log(`Результат множення дорівнює ${x * y * z}`);
+// }
+
+// // 2. Передача аргументів
+// multiply(2, 3, 5); // Результат множення дорівнює 30
+// multiply(4, 8, 12); // Результат множення дорівнює 384
+// multiply(17, 6, 25); // Результат множення дорівнює 2550
+
+// function multiply(x, y, z) {
+//   console.log('Код до return виконується звичайним чином');
+
+//   // Повертаємо результат виразу множення
+
+//   return x * y * z;
+//   console.log('Цей лог ніколи не виконається, він стоїть після return');
+// }
+
+// // Результат роботи функції можна зберегти у змінну
+// let result = multiply(2, 3, 5);
+// console.log(result); // 30
+
+// result = multiply(4, 8, 12);
+// console.log(result); // 384
+
+// result = multiply(17, 6, 25);
+// console.log(result); // 2550
+
+// function multiply(x, y, z) {
+//   console.log(`Результат множення дорівнює ${x * y * z}`);
+// }
+
+// console.log('Лог до виклику функції multiply');
+// multiply(2, 3, 5); // Результат множення дорівнює 30
+// console.log('Лог після виклику функції multiply');
+
+// function count(countFrom = 0, countTo = 10, step = 1) {
+//   console.log(`countFrom = ${countFrom}, countTo = ${countTo}, step = ${step}`);
+
+//   for (let i = countFrom; i <= countTo; i += step) {
+//     console.log(i);
+//   }
+// }
+// console.log(count);
+// count(1, 5); // countFrom = 1, countTo = 5, step = 1
+// count(2); // countFrom = 2, countTo = 10, step = 1
+// count(); // countFrom = 0, countTo = 10, step = 1
+
+// function multiply() {
+//   let total = 1;
+
+//   for (const argument of arguments) {
+//     total *= argument;
+//   }
+
+//   return total;
+// }
+
+// console.log(multiply(1, 2, 3)); //  6
+// console.log(multiply(1, 2, 3, 4)); //  24
+// console.log(multiply(1, 2, 3, 4, 5)); //  120
+
+// function fn() {
+//   // Змінна args буде містити повноцінний масив
+//   const args = Array.from(arguments);
+//   console.log(args);
+// }
+
+// function fn(...args) {
+//   // Змінна args буде містити повноцінний масив
+//   return;
+// }
+// console.log(fn());
+// fn(1, 3, 5, 6, 7);
+// fn(4, 6, 7, 8, 8, 9);
+
+/* 
+    ## Example 8 - Форматування часу
+    Напиши функцію `formatTime(minutes)` яка переведе значення `minutes`
+    (кількість хвилин) у рядок у форматі годин та хвилин `HH:MM`.
+    const totalMinutes = 157;
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    console.log(hours);
+    console.log(minutes);
+    const doubleDigitHours = String(hours).padStart(2, 0);
+    const doubleDigitMinutes = String(minutes).padStart(2, 0);
+    console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
+    function formatTime(minutes) {}
+    console.log(formatTime(70)); // "01:10"
+    console.log(formatTime(450)); // "07:30"
+    console.log(formatTime(1441)); // "24:01"
+*/
+
+/* 
+    ## Example 9 - Колекція курсів (includes, indexOf, push і т. д.)
+    Напишіть функції для роботи з колекцією навчальних курсів `courses`:
+    - `addCourse(name)` - додає курс до кінця колекції
+    - `removeCourse(name)` - видаляє курс із колекції
+    - `updateCourse(oldName, newName)` - змінює ім'я на нове
+    const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL'];
+    addCourse('Express');
+    console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
+    addCourse('CSS'); // 'Ви вже маєте такий курс'
+    removeCourse('React');
+    console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
+    removeCourse('Vue'); // 'Курс із таким ім'ям не знайдено'
+    updateCourse('Express', 'NestJS');
+    console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+*/
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// book.pageCount = 836;
+// book.originalLanguage = 'en';
+// book.translations = ['ua', 'ru'];
+
+// console.log(book.pageCount); // 836
+// console.log(book.originalLanguage); // 'en'
+// console.log(book.translations); // ['ua', 'ru']
+// console.log(book);
+
+// const bookShelf = {
+//   books: ['The Last Kingdom'],
+//   getBooks() {
+//     return this.books;
+//   },
+//   addBook(bookName) {
+//     this.books.push(bookName);
+//   },
+//   removeBook(bookName) {
+//     const bookIndex = this.books.indexOf(bookName);
+//     this.books.splice(bookIndex, 1);
+//   },
+// };
+
+// console.log(bookShelf.getBooks()); // ["The Last Kingdom"]
+// bookShelf.addBook('The Mist');
+// bookShelf.addBook('Dream Guardian');
+// console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'The Mist', 'Dream Guardian']
+// bookShelf.removeBook('The Mist');
+// console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'Dream Guardian']
+
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+
+// for (const key of keys) {
+//   // Ключ
+//   console.log(key);
+//   // Значення властивості
+//   console.log(book[key]);
+//}
+
+// function multiply(firstNumber, secondNumber, ...otherArgs) {
+//   console.log(firstNumber); // Значення першого аргументу
+//   console.log(secondNumber); // Значення другого аргументу
+//   console.log(otherArgs); // Масив інших аргументів
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+//
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: 'Speed potion', price: 460 },
+//     { name: 'Dragon breath', price: 780 },
+//     { name: 'Stone skin', price: 520 },
+//   ],
+//   // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     for (const potion of this.potions) {
+//       if (potion.name === newPotion.name) {
+//         return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//       }
+//     }
+//     const newProduct = {
+//       ...newPotion,
+//     };
+
+//     this.potions.push(newPotion);
+//   },
+//   removePotion(potionName) {
+//     for (let i = 0; i < this.potions.length; i += 1) {
+//       const potion = this.potions[i];
+//       if (potionName === potion.name) {
+//         this.potions.splice(i, 1);
+//       }
+//     }
+//   },
+//   updatePotionName(oldName, newName) {
+//     let resalt = `Potion ${oldName} is not in inventory`;
+//     for (let i = 0; i < this.potions.length; i += 1) {
+//       const potion = this.potions[i];
+
+//       if (oldName === potion.name) {
+//         potion.name = newName;
+//       }
+//     }
+//     return `Found ${oldName} change to ${newName} `;
+//   },
+//   // Change code above this line
+// };
